53010852c3af2b46dd88a17d0fe4ff42437058bd
Stephen Lineker-Miller <slinekermiller@gmail.com>
2021-11-28 19:57:16 +0000

    feat: upnp get mute, set media and play media

--- pkg/samsung-tv-api/soap/client.go
+++ pkg/samsung-tv-api/soap/client.go
@@ -19,7 +19,7 @@ type SamsungSoapClient struct {
 	BaseUrl *url.URL
 }
 
-func (s *SamsungSoapClient) MakeRestRequest(action, arguments, protocol string, output interface{}) error {
+func (s *SamsungSoapClient) MakeSoapRequest(action, arguments, protocol string, output interface{}) error {
 	u := fmt.Sprintf("%s%s1", s.BaseUrl.String(), protocol)
 
 	body := fmt.Sprintf("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"+
@@ -65,20 +65,58 @@ func (s *SamsungSoapClient) MakeRestRequest(action, arguments, protocol string, 
 	return json.Unmarshal(content.Bytes(), &output)
 }
 
-func (s *SamsungSoapClient) GetVolume() (string, error) {
-	log.Println("Get device info via saop api")
+// GetCurrentVolume returns the value of the current volume level
+func (s *SamsungSoapClient) GetCurrentVolume() (string, error) {
+	log.Println("Get device volume via saop api")
 
-	output := GetVolumeResponse{}
-	err := s.MakeRestRequest("GetVolume", "<Channel>Master</Channel>", "RenderingControl", &output)
+	output := GetDeviceVolumeResponse{}
+	err := s.MakeSoapRequest("GetCurrentVolume", "<Channel>Master</Channel>", "RenderingControl", &output)
 
 	return output.Envelope.Body.GetVolumeResponse.CurrentVolume, err
 }
 
+// SetVolume will update the current volume of the display to the provided value.
 func (s *SamsungSoapClient) SetVolume(volume int) error {
-	log.Println("Get device info via soap api")
+	log.Printf("set the volume of the tv to %d via soap api\n", volume)
 
 	var output interface{}
 
 	args := fmt.Sprintf("<Channel>Master</Channel><DesiredVolume>%d</DesiredVolume>", volume)
-	return s.MakeRestRequest("SetVolume", args, "RenderingControl", &output)
+	return s.MakeSoapRequest("SetVolume", args, "RenderingControl", &output)
+}
+
+// GetCurrentMuteStatus returns true if and only if the TV is currently muted
+func (s *SamsungSoapClient) GetCurrentMuteStatus() (bool, error) {
+	log.Println("Get device mute status via saop api")
+
+	output := GetCurrentMuteStatusResponse{}
+	err := s.MakeSoapRequest("GetMute", "<Channel>Master</Channel>", "RenderingControl", &output)
+
+	if err != nil {
+		return false, err
+	}
+
+	return output.Envelope.Body.GetMuteResponse.CurrentMute == "1", err
+}
+
+// SetCurrentMedia will tell the display to play the current media via the URL.
+func (s *SamsungSoapClient) SetCurrentMedia(url string) error {
+	args := fmt.Sprintf("<CurrentURI>%s</CurrentURI><CurrentURIMetaData></CurrentURIMetaData>", url)
+
+	var output interface{}
+	var err error
+
+	err = s.MakeSoapRequest("SetAVTransportURI", args, "AVTransport", &output)
+
+	if err != nil {
+		return err
+	}
+
+	return s.PlayCurrentMedia()
+}
+
+// PlayCurrentMedia will attempt to play the current media already set on the display.
+func (s *SamsungSoapClient) PlayCurrentMedia() error {
+	var output interface{}
+	return s.MakeSoapRequest("Play", "<Speed>1</Speed>", "AVTransport", &output)
 }
